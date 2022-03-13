2688720f2a8b8047838a0f99875e139a33f3eb92
ljymarcus <maplecow@gmail.com>
2022-03-13 19:16:50 +0800

    Added computerPlay function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function computerPlay (){
-    
+    const selection = ["Rock", "Paper", "Scissors"];
+    return selection[Math.floor(Math.random()* selection.length)];
 }
+
+console.log (computerPlay())
